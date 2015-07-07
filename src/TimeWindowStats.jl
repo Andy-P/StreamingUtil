4c18061bf8b0d1e51716da4d61ac124c9c5448ce
Andy-P <andre@quantixresearch.com>
2015-07-07 19:10:03 +0900

    Eliminated unshift! & Switched DateTime -> Dates
    1. Switched DateTime -> Dates
    2. Eliminated unshift! to reduce garbage collection

--- src/TimeWindowStats.jl
+++ src/TimeWindowStats.jl
@@ -1,6 +1,6 @@
 module TimeWindowStats
 
-using Datetime
+using Dates
 
 export Sum,Pct,Mean,update!
 
@@ -8,39 +8,51 @@ abstract TimeWindowStatistic
 
 type Sum{T} <:TimeWindowStatistic
     tm::Int64
+    lastIdx::Int64
     xs::Vector{T}
     tms::Vector{DateTime}
     v::T # value
-    Sum(time::Int64) = new(time,T[],DateTime[],zero(T))
+    Sum(time::Int64) = new(time,0,T[],DateTime[],zero(T))
 end
 
-function update!{T}(stat::Sum, v::T, tm::DateTime)
+function update!{T}(stat::Sum{T}, v::T, tm::DateTime)
     n = length(stat.xs)
     reduced = zero(T)
-    while (length(stat.xs)>0) && (tm - stat.tms[1] > stat.tm)
-#         println("removing $(stat.tms[1]), $(stat.xs[1])")
-        reduced += stat.xs[1]
-        shift!(stat.tms)
-        shift!(stat.xs)
+    i = 0
+    while i < stat.lastIdx && (tm - stat.tms[i+1] > stat.tm)
+        i += 1
+        reduced += stat.xs[i]
     end
-    stat.v += v - reduced
-    push!(stat.tms,tm)
-    push!(stat.xs,v)
+
+    if i > 0 # shift values if anything needs ot be removed
+        stat.tms[1:stat.lastIdx-i] = stat.tms[1+i:stat.lastIdx]
+        stat.xs[1:stat.lastIdx-i] = stat.xs[1+i:stat.lastIdx]
+        stat.lastIdx -= i
+    end
+
+    stat.lastIdx += 1
+    if stat.lastIdx <= length(stat.tms)
+        stat.tms[stat.lastIdx] = tm
+        stat.xs[stat.lastIdx]  = v
+    else
+        push!(stat.tms,tm)
+        push!(stat.xs,v)
+    end
+
+    stat.v += v - reduced # calculate and cache latest value
     return stat
 end
 
 type Mean{T} <:TimeWindowStatistic
     tm::Int64
     n::Sum{T} # nominator
-    d::Sum{T} # denominator (count)
     v::Float64 # value
-    Mean(time::Int64) = new(time, Sum{T}(time), Sum{Int64}(time), zero(T))
+    Mean(time::Int64) = new(time, Sum{T}(time), zero(T))
 end
 
 function update!{T}(stat::Mean, n::T, tm::DateTime)
     update!(stat.n, n, tm)
-    update!(stat.d, 1, tm)
-    stat.v = stat.n.v / stat.d.v
+    stat.v = stat.n.v / stat.n.lastIdx
     return stat
 end
 
@@ -55,7 +67,7 @@ end
 function update!{T}(stat::Pct, n::T, d::T, tm::DateTime)
     update!(stat.n, n, tm)
     update!(stat.d, d, tm)
-    stat.v = stat.d!=0? stat.n.v/stat.d.v:zero(T)
+    stat.v = stat.d!=0? stat.n.v/stat.d.v : zero(T)
     return stat
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,15 @@
 workspace()
 using TimeWindowStats
-using Base.Test, Datetime
+using Base.Test, Dates
 # reload("StreamingUtil")
-
-dt1 = datetime(2015,6,4,7,1,1)
-dt2 = dt1 + seconds(2)
-dt3 = dt1 + seconds(4)
-dt4 = dt1 + seconds(6)
+dt1 = DateTime(2015,6,4,7,1,1)
+dt2 = dt1 + Second(2)
+dt3 = dt1 + Second(4)
+dt4 = dt1 + Second(6)
+dt5 = dt1 + Second(10)
 
 c = Sum{Float64}(5000)
 update!(c,1., dt1)
-length(c.xs)
 @test length(c.xs)== 1
 @test length(c.tms)== 1
 @test c.v == 1.
@@ -32,10 +31,16 @@ update!(c,4., dt4)
 @test length(c.tms)== 3
 @test c.v == 9.
 
-update!(c,3., dt4)
+update!(c,5., dt4)
 @test length(c.xs)== 4
 @test length(c.tms)== 4
-@test c.v == 12.
+@test c.v == 14.
+@test c.lastIdx == 4
+
+update!(c,6., dt5)
+@test length(c.xs)== 4
+@test length(c.tms)== 4
+@test c.lastIdx == 3
 
 c = Sum{Int64}(5000)
 update!(c,1, dt1)
@@ -57,7 +62,7 @@ update!(c,1, dt1)
 @test c.v == 1.
 update!(c,2, dt2)
 @test c.v == 1.5
-update!(c,3., dt3)
+update!(c,3, dt3)
 @test c.v == 2.
-update!(c,4., dt4)
+update!(c,4, dt4)
 @test c.v == 3.
